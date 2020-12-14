fe5ca95cf5c4c792e1eea6d5a7db2a2c615d31a4
jaredbryan881 <jared.bryan1998@gmail.com>
2020-12-14 14:23:50 -0500

    Added documentation for scattering functions

--- src/transform/scatter1d.jl
+++ src/transform/scatter1d.jl
@@ -1,3 +1,15 @@
+"""
+    scatter1d_layer(S, i, x_hat, Transform, FilterBanks)
+
+Apply one layer of the wavelet scattering transform, calling future layers recursively.
+
+# Arguments
+ - `S::Scattered`: Scattered object for holding scattering coefficients.
+ - `i::Int64`: Index of the current scattering layer.
+ - `x_hat::Vector{Complex{Float64}}`: Fourier transformed input data vector.
+ - `Transform::ScatteringTransform1d`: ScatteringTransform1d object containing information about the total transform.
+ - `FilterBanks::Vector{FilterBank1d}`: Filter banks for each layer of the scattering transform.
+"""
 function scatter1d_layer(S::Scattered, i::Int64, x_hat::Vector{Complex{Float64}}, Transform::ScatteringTransform1d, FilterBanks::Vector{FilterBank1d})
     @assert size(S.Coeff[i+1], 2) == length(FilterBanks[i].Λ) "Array of scattering coefficients doesn't have the same dimensions as the number of filters.
                                                                Got $(size(S.Coeff[i+1], 2)) and $(length(FilterBanks[i].Λ)), respectively."
@@ -16,7 +28,16 @@ function scatter1d_layer(S::Scattered, i::Int64, x_hat::Vector{Complex{Float64}}
     end
 end
 
+"""
+    scatter1d(x, Transform, FilterBanks)
 
+Calculate the 1d wavelet scattering transform.
+
+# Arguments
+ - `x::Vector{Float64}`: Time series.
+ - `Transform::ScatteringTransform1d`: Scattering transform object.
+ - `FilterBanks::Vector{FilterBank1d}`: Filter banks for each layer of the scattering transform.
+"""
 function scatter1d(x::Vector{Float64}, Transform::ScatteringTransform1d, FilterBanks::Vector{FilterBank1d})
     @assert length(FilterBanks)==Transform.D "The number of filter banks should equal the scattering transform depth.
                                               Got $(length(FilterBanks)) filter banks and $(Transform.D) layers."
