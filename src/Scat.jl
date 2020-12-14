68fa6ea11084314f6940d8fe549bb62453452e81
jaredbryan881 <jared.bryan1998@gmail.com>
2020-12-14 14:18:15 -0500

    Removed unnecessary import, added 1d scattering transform includes to the main project file.

--- src/Scat.jl
+++ src/Scat.jl
@@ -1,9 +1,10 @@
 module Scat
 
 using FFTW
-using Wavelets
 
 include("./filters/filter.jl")
 include("./filters/filterBank.jl")
 include("./transform/scatteringTransform.jl")
+include("./transform/scattered.jl")
+include("./transform/scatter1d.jl")
 end
