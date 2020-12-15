9f41188fbdc155056463246e56017d955d124302
jaredbryan881 <jared.bryan1998@gmail.com>
2020-12-15 18:16:07 -0500

    Removed unnecessary argument in Scattered1d constructor

--- src/transform/scattered.jl
+++ src/transform/scattered.jl
@@ -19,12 +19,13 @@ struct Scattered1d <: Scattered
     nFilters::Vector{Int64} # number of filters in each layer
     Coeff::Vector{Array{Float64,2}} # scattering coefficients for each layer
 
-    function Scattered1d(T::Int64, F::Vector{FilterBank1d})
+    function Scattered1d(F::Vector{FilterBank1d})
         nFilters = Vector{Int64}(undef, length(F)+1)
         Coeff = Vector{Array{Float64,2}}(undef, length(F)+1)
 
         # 0th order scattering coefficients
         nFilters[1] = 1
+        T = 2^F[1].N
         Coeff[1] = Array{Float64,2}(undef, T, 1)
         # higher order scattering coefficients
         for i=1:length(F)
