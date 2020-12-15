977df721cdc7c3dd858cff4426b0359704f2aef4
jaredbryan881 <jared.bryan1998@gmail.com>
2020-12-15 17:19:35 -0500

    Added additional fields to ScatteringTransform1d

--- src/transform/scatteringTransform.jl
+++ src/transform/scatteringTransform.jl
@@ -1,7 +1,22 @@
+export ScatteringTransform1d
+
 abstract type ScatteringTransform end
 
+"""
+    ScatteringTransform1d
+
+A structure for the wavelet scattering transform for 1d signals.
+
+## Fields: ScatteringTransform1d
+ | **Field** | **Description** |
+ |:----------|:----------------|
+ | :D        | number of layers |
+ | :Q        | Number of wavelets per octave |
+ | :J        | log2(max filter scale) |
+"""
 struct ScatteringTransform1d <: ScatteringTransform
     D::Int # depth/number of layers
-    J::Int # number of scales/number of wavelets in the filterbank
+    Q::Vector{Int} # number of filters per octave for each layer
+    J::Vector{Int} # log2 maximum scale of filters for each layer
 end
 depth(S::ScatteringTransform1d) = S.D
