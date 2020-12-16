e785ee55899827249ae00c4f54ae24ddc2490950
jaredbryan881 <jared.bryan1998@gmail.com>
2020-12-16 11:43:36 -0500

    Allow creation of Scattered from FilterBank1dBlock, fix the size of higher order scattering coefficient arrays

--- src/transform/scattered.jl
+++ src/transform/scattered.jl
@@ -28,9 +28,30 @@ struct Scattered1d <: Scattered
         T = 2^F[1].N
         Coeff[1] = Array{Float64,2}(undef, T, 1)
         # higher order scattering coefficients
+        nVect=1
         for i=1:length(F)
-            Coeff[i+1] = Array{Float64,2}(undef, T, length(F[i].Λ))
             nFilters[i+1] = length(F[i].Λ)
+            nVect=nVect*length(F[i].Λ)
+            Coeff[i+1] = Array{Float64,2}(undef, T, nVect)
+        end
+
+        return new(T, nFilters, Coeff)
+    end
+
+    function Scattered1d(F::Vector{FilterBank1dBlock})
+        nFilters = Vector{Int64}(undef, length(F)+1)
+        Coeff = Vector{Array{Float64,2}}(undef, length(F)+1)
+
+        # 0th order scattering coefficients
+        nFilters[1] = 1
+        T = 2^F[1].N
+        Coeff[1] = Array{Float64,2}(undef, T, 1)
+        # higher order scattering coefficients
+        nRows=1
+        for i=1:length(F)
+            nFilters[i+1] = size(F[i].Λ, 2)
+            nRows=nRows*size(F[i].Λ, 2)
+            Coeff[i+1] = Array{Float64,2}(undef, T, nRows)
         end
 
         return new(T, nFilters, Coeff)
